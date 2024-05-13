980d47e9ea369de1fb9d3880b3968e7d4c5b6042
Alf <42708476+alfset@users.noreply.github.com>
2024-05-13 13:05:11 +0700

    Create config.js

---
+++ IBC-Everywhere/relayer/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    ethRpcUrl: 'wss://mainnet.infura.io/ws/v3/YOUR_INFURA_PROJECT_ID',
+    solanaRpcUrl: "https://api.mainnet-beta.solana.com",
+    cosmosRpcUrl: "https://rpc.cosmos.network:26657",
+    ethAbi: [...],
+    ethContractAddress: '0x...',
+    solanaKeypair: {...}, 
+    cosmosWallet: {...}, 
+};
