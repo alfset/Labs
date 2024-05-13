5b0d844a1696bc87b86552090df26b890ade03cf
Alf <42708476+alfset@users.noreply.github.com>
2024-05-14 03:51:43 +0700

    Update Factory.sol

--- DEFI/DEX/Factory.sol
+++ DEFI/DEX/Factory.sol
@@ -1,1 +1,20 @@
-test
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "./LiquidityPool.sol";
+
+contract LiquidityPoolFactory {
+    address public oracle;
+    event PoolCreated(address indexed tokenA, address indexed tokenB, address poolAddress);
+
+    constructor(address _oracle) {
+        oracle = _oracle;
+    }
+
+    function createPool(address tokenA, address tokenB, string memory symbolTokenA, string memory symbolTokenB) public returns (address) {
+        LiquidityPool pool = new LiquidityPool(oracle, tokenA, tokenB, symbolTokenA, symbolTokenB);
+        emit PoolCreated(tokenA, tokenB, address(pool));
+        return address(pool);
+    }
+}
